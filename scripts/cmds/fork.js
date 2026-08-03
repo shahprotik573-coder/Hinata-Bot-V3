31d0f3205ae77292453e34a29cdcb2c5521ea504
shahprotik573-coder <shahprotik573@gmail.com>
2026-08-04 02:57:38 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -24,8 +24,8 @@ module.exports = {
                         return api.sendMessage("You are not authorized to change the author name.", event.threadID, event.messageID);
                 }
 
-                const githubLink = "https://github.com/mahmudx7/Hinata-Bot-v3";
-                const youtubeLink = "https://youtu.be/zJsemXLaRbY?si=8O-O-nSXgQlsNvnU";
+                const githubLink = "https://github.com/protik/ABLO-Bot-v3";
+                const youtubeLink = "https://youtu.plz ck the vpn connection";
 
                 const response = `✓ | Fork this project here:\n` +
                                  `•${githubLink}\n\n` +
