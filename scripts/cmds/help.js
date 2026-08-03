fd17219a6044f1bea7921b481ee932b404fcc745
shahprotik573-coder <shahprotik573@gmail.com>
2026-08-04 02:49:57 +0600

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -63,7 +63,7 @@ module.exports = {
                                        `Type ${prefix}help <cmd> to see details.`;
 
                         msg += `\n\n⭔ Total Commands: ${totalCommands}\n⭔ ${helpHint}\n`;
-                        msg += `\n╭─✦ ADMIN: MahMUD 彡\n├‣ WHATSAPP\n╰‣ 01836298139`;
+                        msg += `\n╭─✦ owner: shah ogre\n├‣ instagram\n╰‣protiksha`;
 
                         try {
                                 const hh = await message.reply({ body: msg });
